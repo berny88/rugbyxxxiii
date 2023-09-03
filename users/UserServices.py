70396c98255cf7c361cc87b83c2bdacfa066cab3
berny <bernard.bougeon@gmail.com>
2023-09-03 08:45:09 +0200

    xx

--- users/UserServices.py
+++ users/UserServices.py
@@ -189,7 +189,7 @@ def subscriptionPost():
     user = mgr.getUserByEmail(email)
     tool_mgr = ToolManager()
 
-    #url_root = tool_mgr.getProperty("url_root")["value"]
+    #url_root = tool_mgr.getProperty("url_root")["value"] 
     url_root=""
     if user is None:
         logger.info("subscriptionPost::Email {} unknown - user to be created".format(email))
